6107747aec53b1a6913089fe5bc483a90d4a5277
Arjen Smit <arjen@alnitak.nl>
2016-06-10 07:24:43 +0200

    add visuals to grid

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ var Grid = require('./model/Grid');
 
 var myGrid = new Grid();
 
-myGrid.generateTiles(2);
+myGrid.generateTiles(3);
 
 console.log(myGrid.getTiles().length);
 
-myGrid.getTiles().forEach(function (tile) {
+var tiles = myGrid.getTiles();
+var t = 0;
+
+tiles.forEach(function (tile) {
+    t++;
     tile.printPosition();
+    var elemDiv = document.createElement('div');
+    var color = 'rgb(255,0,' + Math.round((255/tiles.length) * t) + ')';
+    elemDiv.style.cssText = 'position:absolute;top:' + ((tile.position.getY() * 20) + 600) + 'px;left:' + ((tile.position.getX() * 20) + 600) + 'px;width:10px;height:10px;background:'+color+';';
+
+
+    document.body.appendChild(elemDiv);
 });
